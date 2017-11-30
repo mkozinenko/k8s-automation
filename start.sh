67a138dc8b882732c96910e65fc9cfec65468039
Maxim Kozinenko <mkozin@softserveinc.com>
2017-11-30 19:55:36 +0200

    initial version

--- start.sh
+++ start.sh
@@ -16,12 +16,12 @@ kubectl apply -f ../kubernetes/jenkins-deployment.yaml
 kubectl rollout status deployment/jenkins
 kubectl port-forward $(kubectl get po | grep jenkins-master | awk '{print $1}') 8080:8080 && curl -XPOST http://admin:admin@localhost:8080/job/springboot_demo/build?token=jenkinsToken
 
-job_status=`curl http://admin:admin@localhost:8080/job/springboot_demo/lastBuild/api/json | grep "\"result\":\"SUCCESS\""`
+#job_status=`curl http://admin:admin@localhost:8080/job/springboot_demo/lastBuild/api/json | grep "\"result\":\"SUCCESS\""`
 
-if [ -n "$job_status" ]
-then
+#if [ -n "$job_status" ]
+#then
     # Run your script commands here
-else
-  echo "BUILD FAILURE: Other build is unsuccessful or status could not be obtained."
-  exit 1
-fi
+#else
+#  echo "BUILD FAILURE: Other build is unsuccessful or status could not be obtained."
+#  exit 1
+#fi
