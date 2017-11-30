7d83d18c720eeeb7b73e9b15d888edd86ed2776c
Maxim Kozinenko <mkozin@softserveinc.com>
2017-11-30 21:57:07 +0200

    initial version

--- start.sh
+++ start.sh
@@ -9,12 +9,16 @@ mv ~/.kube/config ~/.kube/config.pre-vagrant
 
 NODE_MEM=2048 NODE_CPUS=1 NODES=2 MASTER_CPUS=1 USE_KUBE_UI=true DOCKER_OPTIONS="--insecure-registry=registry.default.svc.cluster.local:5000" vagrant up
 
+
+
+kubectl apply -f ././kubernetes/etcd-service.yml
 kubectl apply -f ../kubernetes/registry-deployment.yaml
 kubectl apply -f ../kubernetes/jenkins-deployment.yaml
 
 
 kubectl rollout status deployment/jenkins-master
-kubectl port-forward $(kubectl get po | grep jenkins-master | awk '{print $1}') 8080:8080& curl -XPOST http://admin:admin@localhost:8080/job/springboot_demo/build?token=jenkinsToken
+sleep 15
+kubectl port-forward $(kubectl get po | grep jenkins-master | awk '{print $1}') 8080:8080 && curl -XPOST http://admin:admin@localhost:8080/job/springboot_demo/build?token=jenkinsToken
 
 #job_status=`curl http://admin:admin@localhost:8080/job/springboot_demo/lastBuild/api/json | grep "\"result\":\"SUCCESS\""`
 
