a35447f51ea66af17091fd514c25488152d212c7
Maxim Kozinenko <mkozin@softserveinc.com>
2017-11-30 19:55:21 +0200

    initial version

--- start.sh
+++ start.sh
@@ -7,16 +7,16 @@ git clone https://github.com/pires/kubernetes-vagrant-coreos-cluster.git
 cd kubernetes-vagrant-coreos-cluster
 mv ~/.kube/config ~/.kube/config.pre-vagrant
 
-NODE_MEM=2048 NODE_CPUS=1 NODES=2 MASTER_CPUS=1 USE_KUBE_UI=true DOCKER_OPTIONS="--insecure-registry=registry:5000" vagrant up
+NODE_MEM=2048 NODE_CPUS=1 NODES=2 MASTER_CPUS=1 USE_KUBE_UI=true DOCKER_OPTIONS="--insecure-registry=registry.default.svc.cluster.local:5000" vagrant up
 
 kubectl apply -f ../kubernetes/registry-deployment.yaml
 kubectl apply -f ../kubernetes/jenkins-deployment.yaml
 
 
-#kubectl rollout status deployment/jenkins
-#kubectl port-forward $(kubectl get po | grep jenkins | awk '{print $1}') 8080:8080 && curl -XPOST http://localhost:8080/job/springboot_demo/build?token=jenkinsToken
+kubectl rollout status deployment/jenkins
+kubectl port-forward $(kubectl get po | grep jenkins-master | awk '{print $1}') 8080:8080 && curl -XPOST http://admin:admin@localhost:8080/job/springboot_demo/build?token=jenkinsToken
 
-job_status=`curl http://jenkins:8080/view/job/other-job/lastBuild/api/json | grep "\"result\":\"SUCCESS\""`
+job_status=`curl http://admin:admin@localhost:8080/job/springboot_demo/lastBuild/api/json | grep "\"result\":\"SUCCESS\""`
 
 if [ -n "$job_status" ]
 then
