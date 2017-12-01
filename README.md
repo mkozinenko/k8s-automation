5c1302fbc3ebdcfb93c3bcba3d2fa2798d1daeba
Maxim Kozinenko <mkozin@softserveinc.com>
2017-12-01 13:16:33 +0200

    fixes

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # k8s-automation
 
-make sure you have latest Vagrant, VirtualBox, git for your OS installed.
+### requirements
+
+* make sure you have latest Vagrant, VirtualBox, git for your OS installed.
+* This was tested on Mac OS 10.13.1, but should work on Linux boxes too
+* Make sure you have at least 8GB RAM and 4 cores, as solution needs 5GB and 3 cores for it's VMs
+
+### startup:
+
+```bash
+git clone https://github.com/mkozinenko/k8s-automation.git
+
+cd k8s-automation
+
+./start.sh
+```
+
+You will be prompted for host sudo password to mount NFS volumes (modify /etc/exports in Mac OS case)
+
+### operating:
+
+you can use kubectl to operate created K8s cluster. All app-related stuff is deployed in 'default' namespace
+
+you can manually map Jenkins pod and check/trigger job 
+
+### cleanup:
+
+```bash
+cd kubernetes-vagrant-coreos-cluster
+NODES=2 vagrant destry -f
+```
+
+you will be prompted for host sudo password to unmount NFS volumes (modify /etc/exports in Mac OS case)
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export NODE_COUNT=2
+
 rm -rf kubernetes-vagrant-coreos-cluster
 
 git clone https://github.com/pires/kubernetes-vagrant-coreos-cluster.git
@@ -7,25 +9,46 @@ git clone https://github.com/pires/kubernetes-vagrant-coreos-cluster.git
 cd kubernetes-vagrant-coreos-cluster
 mv ~/.kube/config ~/.kube/config.pre-vagrant
 
-NODE_MEM=2048 NODE_CPUS=1 NODES=2 MASTER_CPUS=1 USE_KUBE_UI=true DOCKER_OPTIONS="--insecure-registry=registry.default.svc.cluster.local:5000" vagrant up
+NODE_MEM=2048 NODE_CPUS=1 NODES=$NODE_COUNT MASTER_CPUS=1 USE_KUBE_UI=true DNS_PROVIDER=coredns DOCKER_OPTIONS="--insecure-registry=registry.default.svc.cluster.local:5000" vagrant up
 
 
 
-kubectl apply -f ../kubernetes/calico-service.yml
+#kubectl apply -f ../kubernetes/calico-service.yml
 kubectl apply -f ../kubernetes/registry-deployment.yaml
 kubectl apply -f ../kubernetes/jenkins-deployment.yaml
 
-
 kubectl rollout status deployment/jenkins-master
-sleep 15
-kubectl port-forward $(kubectl get po | grep jenkins-master | awk '{print $1}') 8080:8080 && curl -XPOST http://admin:admin@localhost:8080/job/springboot_demo/build?token=jenkinsToken
 
-#job_status=`curl http://admin:admin@localhost:8080/job/springboot_demo/lastBuild/api/json | grep "\"result\":\"SUCCESS\""`
+sleep 20
+export DNS_IP=`kubectl get svc kube-dns --namespace=kube-system | awk 'NR>1{print $2}'`
 
-#if [ -n "$job_status" ]
-#then
-    # Run your script commands here
-#else
-#  echo "BUILD FAILURE: Other build is unsuccessful or status could not be obtained."
-#  exit 1
-#fi
+vagrant ssh master -c "echo nameserver=$DNS_IP |sudo tee -a /etc/resolv.conf"
+for ((i=1; i<=$NODE_COUNT;i++)) ;do vagrant ssh node-0$i -c "echo nameserver=$DNS_IP |sudo tee -a /etc/resolv.conf";done
+
+kubectl port-forward $(kubectl get po | grep jenkins-master | awk '{print $1}') 8080:8080 2>&1 >/dev/null&
+
+
+echo "Triggering buildjob when Jenkins will be ready..."
+sleep 120
+
+job_started="When job will be started successfuly, this env will be empty"
+until [ -z "$job_started" ]; do job_started=`curl -s -XPOST http://admin:admin@localhost:8080/job/springboot_demo/build?token=jenkinsToken | grep "<title>"` && sleep 60;done
+
+echo "Checking if build job completed... please wait. You can check progress at http://localhost:8080/job/springboot_demo/"
+
+vagrant ssh master -c "echo nameserver=$DNS_IP |sudo tee -a /etc/resolv.conf"
+for ((i=1; i<=$NODE_COUNT;i++)) ;do vagrant ssh node-0$i -c "echo nameserver=$DNS_IP |sudo tee -a /etc/resolv.conf";done
+
+job_status=''
+until [ -n "$job_status" ]; do job_status=`curl -s http://admin:admin@localhost:8080/job/springboot_demo/lastBuild/api/json | grep "\"buildResult\":\"SUCCESS\""` && sleep 60;done
+
+export PFW_PID=`ps -ef | grep "kubectl port-forward"| awk 'FNR==1{print $2}'`
+echo "Killing Jenkins port-forward process, PID:":$PFW_PID"..."
+kill -9 $PFW_PID
+export SERVICE_PORT=`kubectl get svc springboot-demo -o yaml | grep nodePort | awk '{print $2}'`
+export MASTER_IP=`vagrant ssh master -c "ifconfig eth1 | grep inet" | awk 'FNR == 1 {print $2}'`
+
+curl $MASTER_IP:$SERVICE_PORT
+
+echo "Please, check springboot-demo application at http://"$MASTER_IP":"$SERVICE_PORT
+echo "Kubernetes Dashboard is accessible at http://"$MASTER_IP":8080/ui"
