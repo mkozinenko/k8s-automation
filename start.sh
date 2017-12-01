c15fe015b39fbe2641d8b907caae0f57ec09a8a2
Maxim Kozinenko <mkozin@softserveinc.com>
2017-12-01 13:17:37 +0200

    fixes

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ git clone https://github.com/pires/kubernetes-vagrant-coreos-cluster.git
 cd kubernetes-vagrant-coreos-cluster
 mv ~/.kube/config ~/.kube/config.pre-vagrant
 
-NODE_MEM=2048 NODE_CPUS=1 NODES=$NODE_COUNT MASTER_CPUS=1 USE_KUBE_UI=true DNS_PROVIDER=coredns DOCKER_OPTIONS="--insecure-registry=registry.default.svc.cluster.local:5000" vagrant up
+NODE_MEM=2048 NODE_CPUS=1 NODES=$NODE_COUNT MASTER_CPUS=1 USE_KUBE_UI=true DOCKER_OPTIONS="--insecure-registry=registry.default.svc.cluster.local:5000" vagrant up
 
 
 
